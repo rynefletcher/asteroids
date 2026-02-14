d0e915dfd5ff3c0339fc2254aa01129cf6fa4236
rynefletcher <rynefletcher@pm.me>
2026-02-13 21:40:04 -0600

    2/13 added asteroids

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
-        log_state()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        pygame.display.flip()
-
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids with pygame: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    while True:
+        log_state()
+
+        updatable.update(dt)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
